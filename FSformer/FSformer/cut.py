6a8c8372fe42626f320762ffa7200b5aa5a635e4
HITSZIoT <hitsziot@163.com>
2025-04-10 15:18:17 +0800

    Site updated: 2025-04-10 15:18:14

--- FSformer/FSformer/cut.py
+++ FSformer/FSformer/cut.py
@@ -6,12 +6,14 @@ import soundfile
 import librosa
 import librosa.display
 
-old_path = r'E:\Blog\source\_posts\FSTF-AN\FSTF-AN\clean\target_fileid_2651.wav'
-save_path = r'E:\Blog\source\_posts\FSTF-AN\FSTF-AN\target_fileid_2651.wav'
+old_path = r'E:\Blog\source\_posts\FSformer\FSformer\real\SpatialNet2.wav'
+save_path = r'E:\Blog\source\_posts\FSformer\FSformer\real\SpatialNet2_gai.wav'
+
+zeros = np.zeros(128)
 
 data, samplerate = soundfile.read(old_path)
 
-data_new = data[:64000]
+data_new = np.concatenate((data, zeros))
 
 soundfile.write(save_path, data_new, samplerate)
 
